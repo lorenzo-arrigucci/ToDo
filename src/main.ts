d9106acdef767650c150517465ec2c4905dba9dc
l.arrigucci <arri30@hotmail.it>
2023-03-10 14:38:02 +0100

    import bootstrap

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,7 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+import "bootstrap/scss/bootstrap.scss"
 import './assets/main.css'
 
 const app = createApp(App)
@@ -9,3 +10,5 @@ const app = createApp(App)
 app.use(router)
 
 app.mount('#app')
+
+import "bootstrap/dist/js/bootstrap.js";
